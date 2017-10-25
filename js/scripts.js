95e624722754dd1424c6b2ac87402f5e709efad1
Lieghti <lieghti@gmail.com>
2017-10-25 12:21:57 -0700

    add more to html, js, and a few photos.

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $("form.survey").submit(function(event) {
+    event.preventDefault();
+    var ageRange = $("select.age-range").val();
+    var climate = $("select.climate").val();
+    var shoeType = $("select.shoe-type").val();
+    var season = $("select.season").val();
+    var hairType = $("select.hair-type").val();
+
+  if { ageRange === value 1 or && climate === value 1 or value 2
+
+  }
+    $(".age-range").empty().append(quote);
+    $("#quote").show();
+
+  });
+});
+
+
+//$('select[name=selector]').change(function() { alert($(this).val()); });
