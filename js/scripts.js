e2bf8202efb4595b5f260a59ec71edae351b697a
Guest User <Guest@Epicodus-9D.local>
2017-10-25 18:33:15 -0700

    adding the age range conditional statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,39 @@
+var cold = 0;
+var moderate = 0;
+var hot = 0;
+
 $(document).ready(function() {
-  $("form.survey").submit(function(event) {
-    var ageRangeInput = $("form#age-range").val();
-    var climateInput = $("form#climate").val();
-    var shoeTypeInput = $("form#shoe-type").val();
-    var seasonInput = $("form#season").val();
-    var hairTypeInput = $("form#hair-type").val();
+  $("#survey").submit(function(event) {
+    //debugger;
+    console.log(hot);
+    var ageRangeInput = parseInt($("#age-range").val());
+    // var climateInput = $("#climate").val();
+    // var shoeTypeInput = $("#shoe-type").val();
+    // var seasonInput = $("#season").val();
+    // var hairTypeInput = $("#hair-type").val();
 
+    if (ageRangeInput === 1) {
+      hot += 1;
+    } else if (ageRangeInput === 2) {
+      moderate += 1;
+    }
 
+    //top scores
+    console.log(hot);
+    console.log(moderate);
+    console.log(cold);
 
-  if (ageRangeInput === "Less than 20 years old" && climateInput === "Extreme Heat!" && shoeTypeInput === "Flip Flops" && seasonInput === "Spring" && hairTypeInput === "Bone Straight") {
-      $(".winter").hide();
-      $(".spring").hide();
-      $(".summer").show ();
+  /*if (ageRangeInput === "Less than 20 years old" && climateInput === "Extreme Heat!" && shoeTypeInput === "Flip Flops" && seasonInput === "Spring" && hairTypeInput === "Bone Straight") {
+      $(".cold").hide();
+      $(".moderate").hide();
+      $(".hot").show ();
     }
     else {
-      $(".winter").hide();
-      $(".spring").hide();
-      $(".summer").show ();
+      $(".cold").hide();
+      $(".moderate").hide();
+      $(".hot").show ();
 
-    }
+    }*/
 
     event.preventDefault();
   });
